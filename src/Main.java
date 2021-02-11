19e182fe03f417ee22a12c4215a253a8a1ecda39
Sebastian100795 <78736851+Sebastian100795@users.noreply.github.com>
2021-02-11 15:15:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+       
         int zahl1 = 10;
         int zahl2 = 5;
 
-        int ergebnis = zahl1 + zahl2;
-        System.out.println(zahl1 + " + " +zahl2 + " = " + ergebnis);
-        ergebnis= zahl1-zahl2;
-        System.out.println(zahl1 + "-" + zahl2 + "=" + ergebnis);
-        ergebnis= zahl1*zahl2;
-        System.out.println(zahl1 + "*" +  zahl2 + " = " + ergebnis);
-        ergebnis=zahl1/zahl2;
-        System.out.println(zahl1 + "/" + zahl2 + "=" + ergebnis);
+
+        System.out.printf("%d + %d = %d \n", zahl1, zahl2, zahl1 + zahl2);
+        System.out.printf("%d - %d = %d \n", zahl1, zahl2, zahl1 - zahl2);
+        System.out.printf("%d * %d = %d \n", zahl1, zahl2, zahl1 * zahl2);
+        System.out.printf("%d / %d = %d \n", zahl1, zahl2, zahl1 / zahl2);
 
         double zahl3 = 25.7;
         double zahl4 = 12.5;
 
 
-        System.out.printf("%.3f + %.3f = %.3f \n", zahl3, zahl4, zahl3+zahl4);
-
-        System.out.printf("%.3f - %.3f = %.3f \n",zahl3, zahl4, zahl3-zahl4);
-
-        System.out.printf("%.3f * %.3f = %.3f \n", zahl3,zahl4,zahl3*zahl4);
-
-        System.out.printf("%.3f / %.3f = %.3f \n",zahl3, zahl4, zahl3/zahl4);
+        System.out.printf("%.3f + %.3f = %.3f \n", zahl3, zahl4, zahl3 + zahl4);
+        System.out.printf("%.3f - %.3f = %.3f \n", zahl3, zahl4, zahl3 - zahl4);
+        System.out.printf("%.3f * %.3f = %.3f \n", zahl3, zahl4, zahl3 * zahl4);
+        System.out.printf("%.3f / %.3f = %.3f \n", zahl3, zahl4, zahl3 / zahl4);
 
     }
 
